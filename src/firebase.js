a9ab51e5f3549d3e18354a360b2318b7502fcd40
KC9955 <kinal9955@gmail.com>
2023-02-03 10:59:00 +0530

    COMMIT

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6j-wh_ppmcpipbpqD6dqtRVW_GhLwfC4",
-  authDomain: "linkdin-clone-26ecc.firebaseapp.com",
-  projectId: "linkdin-clone-26ecc",
-  storageBucket: "linkdin-clone-26ecc.appspot.com",
-  messagingSenderId: "424403844446",
-  appId: "1:424403844446:web:e2fc59511a473725d7a3cf",
+  apiKey: "AIzaSyCVd9ZUMGPztOl-2kUpXPdXC6exhiiSbvI",
+  authDomain: "linkdin-clone-using-redux.firebaseapp.com",
+  projectId: "linkdin-clone-using-redux",
+  storageBucket: "linkdin-clone-using-redux.appspot.com",
+  messagingSenderId: "178841180866",
+  appId: "1:178841180866:web:5a1f43599dc5f8e637f95b",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
